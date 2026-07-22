a6cfd55539acc8dec2ecf10f0b1249726a641ce3
odilonsilva <recupera.odih@hotmail.com>
2026-07-22 00:27:02 -0300

    ✨ feat add visualizer background and song info

--- scripts/content.js
+++ scripts/content.js
@@ -1,11 +1,18 @@
 console.log("content loaded");
-setTimeout(() => init(), 5000);
+let playing = false;
+let showingInfo = false;
+let showingInfoInterval = null;
+
+const interval = setInterval(() => init(), 1000);
 
 const init = () => {
   const navBarSpotify = document.querySelector("#global-nav-bar");
+  console.log("tentando encontrar a navbar do spotify");
   if (navBarSpotify) {
     console.log("navBarSpotify localizada");
     createActionButton(navBarSpotify);
+    createVisualizer();
+    clearInterval(interval);
   }
 };
 
@@ -14,20 +21,63 @@ const createActionButton = (container) => {
   button.id = "retro-player-action";
   button.alt = "Ative a visualização media player like";
   button.title = "Ative a visualização media player like";
-  button.width = 35;
-  button.height = 35;
-  button.style = "margin: auto;";
-  button.src =
-    "https://raw.githubusercontent.com/odilonsilva/retro-media-visualizer/refs/heads/main/player.png";
+  button.src = chrome.runtime.getURL("images/player.png");
   container.lastElementChild.appendChild(button);
-  button.addEventListener("click", () => createVisualizer());
+  button.addEventListener("click", () => showVisualizer());
 };
 
 const createVisualizer = () => {
   const container = document.querySelector("body");
   const visualizer = document.createElement("div");
   visualizer.id = "retro-media-visualizer";
-  visualizer.style =
-    "position:absolute; top: 0; left: 0; bottom:0; width: 100vw; height: 100vh; background-color: #fff5";
+
+  const visualizerContent = document.createElement("img");
+  visualizerContent.id = "visualizer-content";
+  visualizerContent.src = chrome.runtime.getURL("images/media-preview.png");
+  visualizer.appendChild(visualizerContent);
   container.appendChild(visualizer);
+};
+
+const visualizerInfo = () => {
+  const container = document.querySelector("#retro-media-visualizer");
+  const visualizerInfo = document.createElement("div");
+  visualizerInfo.id = "retro-media-visualizer-info";
+  const playingInfo = document.querySelector(
+    "[data-testid=now-playing-widget]",
+  );
+
+  if (playingInfo) {
+    visualizerInfo.innerHTML = playingInfo.innerHTML;
+  }
+
+  container.appendChild(visualizerInfo);
+};
+
+const updateVisualizerInfo = () => {
+  const visualizerInfo = document.querySelector("#retro-media-visualizer-info");
+  const playingInfo = document.querySelector(
+    "[data-testid=now-playing-widget]",
+  );
+  if (!visualizerInfo || !playingInfo) return;
+
+  visualizerInfo.innerHTML = playingInfo.innerHTML;
+};
+
+const showVisualizer = () => {
+  const visualizer = document.querySelector("#retro-media-visualizer");
+
+  if (visualizer) {
+    if (visualizer.classList.contains("visualizer-enter")) {
+      visualizer.classList.remove("visualizer-enter");
+      visualizer.classList.add("visualizer-exit");
+      showingInfo = false;
+      clearInterval(showingInfoInterval);
+    } else {
+      visualizer.classList.remove("visualizer-exit");
+      visualizer.classList.add("visualizer-enter");
+      visualizerInfo();
+      showingInfo = true;
+      showingInfoInterval = setInterval(() => updateVisualizerInfo(), 1000);
+    }
+  }
 };
